7da97804fd84d329b25009ddd723e3036a6d1530
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2021-02-06 20:52:01 +0700

    .

--- src/components/SearchScreen/SearchScreen.js
+++ src/components/SearchScreen/SearchScreen.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import React,{useEffect, useState} from 'react'
+import Button from '@material-ui/core/Button';
+
 import "./SearchScreen.css"
 const BILL_API_LINK = "https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/50/query?where=1%3D1&outFields=*&outSR=4326&f=json"
 const SearchScreen = () => {
@@ -7,6 +9,7 @@ const SearchScreen = () => {
     const [value, setValue] = useState("")
     const [agencies, setAgencies] = useState([])
     const [invalidate, setInvalidate] = useState(true)
+    const [agencyNames, setAgencyNames] = useState([])
 
     const onHandleSearch = async (e) => {
         if (e.key === "Enter"){
@@ -21,31 +24,42 @@ const SearchScreen = () => {
     }   
 
     useEffect(() => {
+        let newAgencyNames = []
         axios.get(BILL_API_LINK)
         .then(res => {
+            res.data.features.map((agency) => newAgencyNames.push(agency["attributes"]["AGENCY"]))
+            newAgencyNames = newAgencyNames.filter(function(item, pos) {
+                return newAgencyNames.indexOf(item) == pos;
+            })
+            setAgencyNames(newAgencyNames)
             setAgencies(res.data.features)
         })
         .catch(err => console.log(err))
     }, [])
 
+    console.log(agencyNames)
+
     return (
         <div className="container">
+            <div className="header">
+                <h2>Agency Search</h2>
+            </div>
             <div className={!open ? `search` : `search open`}>
                 <input value={value} onKeyDown={onHandleSearch} onChange={(e) => setValue(e.target.value)} type="search" class="search-box"/>
                 <span className="search-button" onClick={() => setOpen(!open)}>
                     <span className="search-icon"></span>
                 </span>
             </div>
-            <div>
-                {agencies && agencies
-                    .filter((agency) => agency["attributes"]["AGENCY"].includes(value))
+                <ul>
+                {agencyNames && agencyNames
+                    .filter((agency) => agency.trim().toLowerCase().includes(value.trim().toLowerCase()))
                     .map((agency) => {
                         return (
-                            <div className="text-style">{agency["attributes"]["AGENCY"]}</div>
+                            <li className="text-style">{agency}</li>
                         )
                     })
                     }
-                </div>
+                </ul>
         </div>
     )
 }
