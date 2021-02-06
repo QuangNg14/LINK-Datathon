9bf99a4830d3b992c8a9db276f468328b906928a
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2021-02-06 22:41:00 +0700

    recommended top 3 vendors

--- src/components/SearchScreen/SearchScreen.js
+++ src/components/SearchScreen/SearchScreen.js
@@ -9,43 +9,69 @@ const SearchScreen = () => {
     const [value, setValue] = useState("")
     const [agencies, setAgencies] = useState([])
     const [invalidate, setInvalidate] = useState(true)
-    const [agencyNames, setAgencyNames] = useState([])
-
-    const onHandleSearch = async (e) => {
-        if (e.key === "Enter"){
-            try{
-                const res = await axios.get(BILL_API_LINK)
-                setAgencies(res.data.features)
-            }
-            catch(err){
-                console.log(err)
-            }
-        }
-    }   
+    const [agencyNames, setAgencyNames] = useState([]) 
+    const [relatedVendors, setRelatedVendors] = useState()
+    const [recommendedVendors, setRecommendedVendors] = useState()
 
     useEffect(() => {
         let newAgencyNames = []
+        let relatedVendors = {}
+        let recommendedVendors = {}
         axios.get(BILL_API_LINK)
         .then(res => {
-            res.data.features.map((agency) => newAgencyNames.push(agency["attributes"]["AGENCY"]))
+            res.data.features.map((agency) => {
+                const agencyName = agency["attributes"]["AGENCY"]
+                const vendorName = agency["attributes"]["VENDOR_NAME"]
+                newAgencyNames.push(agencyName)
+                if (relatedVendors.hasOwnProperty(agencyName)){
+                    if (relatedVendors[agencyName].hasOwnProperty(vendorName)){
+                        relatedVendors[agencyName][vendorName] += 1
+                    }
+                    else{
+                        relatedVendors[agencyName][vendorName] = 1
+                    }
+                }
+                else{
+                    relatedVendors[agencyName] = {}
+                }
+
+            }
+            )
+
+        res.data.features.map((agency) => {
+            const agencyName = agency["attributes"]["AGENCY"]
+            const vendorName = agency["attributes"]["VENDOR_NAME"]      
+            var sortable = [];
+            for (var r in relatedVendors[agencyName]) {
+                sortable.push([r, relatedVendors[agencyName][r]]);
+            }
+            
+            sortable.sort(function(a, b) {
+                return b[1] - a[1];
+            });
+            
+            recommendedVendors[agencyName] = sortable.slice(0, 3)
+        })
+            
             newAgencyNames = newAgencyNames.filter(function(item, pos) {
                 return newAgencyNames.indexOf(item) == pos;
             })
+            setRecommendedVendors(recommendedVendors)
+            setRelatedVendors(relatedVendors)
             setAgencyNames(newAgencyNames)
             setAgencies(res.data.features)
         })
         .catch(err => console.log(err))
     }, [])
 
-    console.log(agencyNames)
-
+    console.log(recommendedVendors)
     return (
         <div className="container">
             <div className="header">
                 <h2>Agency Search</h2>
             </div>
             <div className={!open ? `search` : `search open`}>
-                <input value={value} onKeyDown={onHandleSearch} onChange={(e) => setValue(e.target.value)} type="search" class="search-box"/>
+                <input value={value} onChange={(e) => setValue(e.target.value)} type="search" class="search-box"/>
                 <span className="search-button" onClick={() => setOpen(!open)}>
                     <span className="search-icon"></span>
                 </span>
